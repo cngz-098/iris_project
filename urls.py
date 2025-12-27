e90719eaa61c780e4d27cfa729d7267776779812
cngz-098 <cemkara34098@gmail.com>
2025-12-28 00:34:09 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,28 +1,23 @@
-from django.urls import path, include 
-from . import views
-from django.contrib.auth.views import LogoutView
-from rest_framework.routers import DefaultRouter 
+"""
+URL configuration for iris_project project.
 
-# API için router tanımlıyoruz
-router = DefaultRouter()
-router.register(r'observations', views.IrisObservationViewSet) # Views içindeki ViewSet'i bağladım
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home_or_public, name='home'),
-path('dashboard/', views.home_or_public, name='dashboard'),
-
-    path('signin/', views.signin_view, name='signin'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    
-    path('iris/', views.iris_list, name='iris_list'),
-    path('iris/add/', views.iris_create, name='iris_create'),
-    path('iris/update/<int:pk>/', views.iris_update, name='iris_update'),
-    path('iris/delete/<int:pk>/', views.iris_delete, name='iris_delete'),
-    
-    path('export/', views.export_iris_csv, name='export_csv'),
-    path('import/', views.import_iris_csv, name='import_csv'),
-    path('predict/', views.predict_species, name='predict'),
-
-    path('api/', include(router.urls)), # Tarayıcıda /api/observations/ olarak görünecek
+    path('admin/', admin.site.urls),
+    path('', include('iris_app.urls')),
 ]
